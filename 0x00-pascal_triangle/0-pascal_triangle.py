a0f83af7b84be9a4d968029f36c176122c0e34f2
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-10-07 00:27:22 +0000

    d

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,27 @@
-/*eslint-disable*/
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-  if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
-  }
-  return [task, task2];
-}
+#!/usr/bin/python3
+"""Pascal Triangle Interview Challenge"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    triangle = [0] * n
+
+    for i in range(n):
+        # define a row and fill first and last idx with 1
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(new_row):
+                a = triangle[i - 1][j]
+                b = triangle[i - 1][j - 1]
+                new_row[j] = a + b
+
+        triangle[i] = new_row
+
+    return triangle
