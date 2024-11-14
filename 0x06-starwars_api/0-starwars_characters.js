a0d1f55bcd9a28946bbcaf4f32b946e9b58afdb8
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-14 21:45:14 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,28 +6,22 @@ const util = require('util');
 const requestPromise = util.promisify(request);
 
 const movieId = process.argv[2];
-const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-request(movieUrl, function (error, response, body) {
+request(movieUrl, async function (error, response, body) {
   if (!error && response.statusCode === 200) {
     const filmData = JSON.parse(body);
     const characters = filmData.characters;
-    printCharacters(characters);
-  } else {
-    console.error(error);
-  }
-});
-
-function printCharacters (characters) {
-  (async () => {
-    for (const characterUrl of characters) {
-      try {
+    try {
+      for (const characterUrl of characters) {
         const characterResponse = await requestPromise(characterUrl);
         const characterData = JSON.parse(characterResponse.body);
         console.log(characterData.name);
-      } catch (err) {
-        console.error(err);
       }
+    } catch (err) {
+      console.error(err);
     }
-  })();
-}
+  } else {
+    console.error(`Error: ${response.statusCode} ${response.statusMessage}`);
+  }
+});
