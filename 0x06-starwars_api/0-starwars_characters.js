d3f39a82e2cdc8892df47fc53cb5eac389363622
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-14 21:35:42 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,37 +2,26 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  let count = 0;
-
-  characters.forEach((character) => {
-    request(character, function (error, response, body) {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-      count++;
-      
-      if (count === characters.length) {
-        return;
-      }
-    });
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
   });
-});
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
+  }
+);
