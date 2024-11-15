a971c8d97f495f5ea1b61a978322450e57b46c14
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-15 12:08:44 +0100

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,55 +3,25 @@
 const request = require('request');
 const util = require('util');
 
-// Convert request to use promises
 const requestPromise = util.promisify(request);
 
-// Base URL for the Star Wars API
-const BASE_URL = 'https://swapi.dev/api';
-
-// Get the movie ID from command line arguments
 const movieId = process.argv[2];
+const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-if (!movieId) {
-  console.error('Please provide a movie ID');
-  process.exit(1);
-}
-
-async function getCharacterName(url) {
-  try {
-    const response = await requestPromise(url);
-    const character = JSON.parse(response.body);
-    return character.name;
-  } catch (error) {
-    console.error(`Error fetching character: ${error.message}`);
-    return null;
+request(movieUrl, async function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+    try {
+      for (const characterUrl of characters) {
+        const characterResponse = await requestPromise(characterUrl);
+        const characterData = JSON.parse(characterResponse.body);
+        console.log(characterData.name);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  } else {
+    console.error(`Error: ${response.statusCode} ${response.statusMessage}`);
   }
-}
-
-async function getMovieCharacters(movieId) {
-  try {
-    // Fetch movie data
-    const response = await requestPromise(`${BASE_URL}/films/${movieId}/`);
-    const movie = JSON.parse(response.body);
-    
-    // Get all character URLs from the movie
-    const characterUrls = movie.characters;
-    
-    // Fetch all characters in parallel and maintain order
-    const characterNames = await Promise.all(
-      characterUrls.map(url => getCharacterName(url))
-    );
-    
-    // Print character names, filtering out any null values from errors
-    characterNames
-      .filter(name => name !== null)
-      .forEach(name => console.log(name));
-    
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
-}
-
-// Execute the main function
-getMovieCharacters(movieId);
+});
