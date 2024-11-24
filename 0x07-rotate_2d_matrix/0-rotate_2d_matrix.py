ef47068f13bd8105c78a84886e086a1f8e1526b9
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-11-24 23:30:17 +0100

    Add solution for rotating 2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """
-Module for rotating a 2D matrix 90 degrees clockwise
+Module for rotating a 2D matrix 90 degrees clockwise.
 """
 
 
 def rotate_2d_matrix(matrix):
     """
-    Rotate a n x n 2D matrix 90 degrees clockwise in-place
+    Rotate a n x n 2D matrix 90 degrees clockwise in-place.
     Args:
-        matrix: a n x n 2D matrix
+        matrix: n x n 2D matrix
+    Returns:
+        None
     """
     n = len(matrix)
-    # Transpose matrix
-    for row in range(n):
-        for col in range(row):
-            matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
-
-    # Reverse rows
-    for row in range(n):
-        matrix[row].reverse()
+    
+    for i in range(n):
+        for j in range(i, n):
+            # Swap elements across diagonal
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
