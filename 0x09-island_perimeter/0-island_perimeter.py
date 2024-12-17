8f3b94bea809db589ac0167fd865f216fbba6fb2
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-12-17 14:48:52 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,62 +1,38 @@
 #!/usr/bin/python3
-'''0x09. Island Perimeter'''
+"""Module that calculates the perimeter of an island in a grid."""
 
 
 def island_perimeter(grid):
-    '''returns the perimeter of the island described in grid'''
-    counter = 0
-    grid_max = len(grid) - 1  # index of the last list in the grid
-    lst_max = len(grid[0]) - 1  # index of the last square in list
+    """
+    Calculate the perimeter of the island described in grid.
 
-    for lst_idx, lst in enumerate(grid):
-        for land_idx, land in enumerate(lst):
-            if land == 1:
-                # left and right
-                if land_idx == 0:
-                    # left side
-                    counter += 1
+    Args:
+        grid (list of list of int): 2D grid where:
+            - 0 represents water
+            - 1 represents land
 
-                    # right side
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
-                elif land_idx == lst_max:
-                    # left side
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
+    Returns:
+        int: The perimeter of the island
+    """
+    if not grid:
+        return 0
 
-                    # right side
-                    counter += 1
-                else:
-                    # left side
-                    if lst[land_idx - 1] == 0:
-                        counter += 1
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-                    # right side
-                    if lst[land_idx + 1] == 0:
-                        counter += 1
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Add 4 for each land cell
+                perimeter += 4
+                
+                # Check and subtract for adjacent land cells
+                # Check cell above if it exists
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                # Check cell to the left if it exists
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
 
-                # top and down
-                if lst_idx == 0:
-                    # top side
-                    counter += 1
-
-                    # bottom side
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-                elif lst_idx == grid_max:
-                    # top side
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-
-                    # bottom side
-                    counter += 1
-                else:
-                    # top side
-                    if grid[lst_idx - 1][land_idx] == 0:
-                        counter += 1
-
-                    # bottom side
-                    if grid[lst_idx + 1][land_idx] == 0:
-                        counter += 1
-
-    return counter
+    return perimeter
