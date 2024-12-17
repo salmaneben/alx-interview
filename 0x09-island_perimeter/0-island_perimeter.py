58a2912c6792d997717559091e89d1614f1f2717
salmaneben <salmanebenyakhlaf95@gmail.com>
2024-12-17 15:17:14 +0100

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 """
-Contains island perimeter function
+Module to calculate island perimeter
 """
 
 
 def island_perimeter(grid):
     """
-    Returns the perimeter of the island described in grid
+    Calculate island perimeter from grid
+    Args:
+        grid (list): List of list of integers
+        0 represents water
+        1 represents land
+    Return:
+        Perimeter of the island
     """
     rows = len(grid)
-    cols = len(grid[0])
+    cols = len(grid[0]) if rows else 0
     perimeter = 0
 
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4
-                # subtract 2 for every internal edge
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 0:
+                continue
+                
+            if i == 0 or grid[i-1][j] == 0:    # top
+                perimeter += 1
+            if i == rows-1 or grid[i+1][j] == 0:  # bottom
+                perimeter += 1
+            if j == 0 or grid[i][j-1] == 0:    # left
+                perimeter += 1
+            if j == cols-1 or grid[i][j+1] == 0:  # right
+                perimeter += 1
 
     return perimeter
